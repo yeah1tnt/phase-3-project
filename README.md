bba84399662d30fc5cea699ac165997e81df6bfe
Tong Tang <pichip22@hotmail.com>
2023-08-23 18:01:12 -0400

    Set up layout, set up simple cli to test user input and user choices

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # phase-3-project
+
+1. Set up python environment with 
+```pipenv install sqlalchemy alembic```
+
+2. Set up layout based on project template
+```
+|- Pipfile
+|- Pipfile.lock
+|- README.md
+|- lib
+    |- cli.py
+    |- debug.py
+    |- helpers.py
+    |- db
+        |-models.py
+        |-seed.py
+```
+
+3. Set up a simple layout for the ```cli.py``` with 3 options. Add, search and exit.
--- lib/cli.py
+++ lib/cli.py
@@ -1,9 +1,37 @@
 
 
 class myCLI:
-    def __init__(self, user_input):
-        self.user_input = user_input
+    def __init__(self):
+        self.main()
+
+    def main(self):
+        count = 3
+        user_input = input("Enter a word: ")
+        print(f"\nThe word you entered is: {user_input}")
+        print(f"\nWhat do you want to do with this word?")
+
+        while user_input != "exit":
+            print("\nEnter 'add' to add the word to the dictionary")
+            print("Enter 'search' to search the dictionary")
+            print("Enter 'exit' to exit the program")
+            user_choice = input("Enter your choice: ")
+            if user_choice.lower() == "add":
+                print("\nWord added to the dictionary")
+                #Run function to add word
+                break
+            elif user_choice.lower() == "search":
+                print("\nWord searched in the dictionary")
+                #Run function to search word
+            elif user_choice.lower() == "exit":
+                print("\nProgram exited")
+                #Exit program
+                break
+            else:
+                count -= 1
+                print("\nInvalid input, Please try again, tries left: {}".format(count))
+                if count == 0:
+                    print("\nToo many invalid inputs. Terminating program")
+                    break
 
 if __name__ == '__main__':
-    user_input = input("Enter a word: ")
-    
+    myCLI()
